7a02be63274db7ee27ef3b6a2fe37761174cc8e2
Yasmin Haque <yasmin.haque@baringa.com>
2020-10-15 22:24:29 +0100

    Added client, user models and user schema fixtures for unit and integration tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,52 @@
 import pytest
 
 from app.app import create_app
-from app.models import db
+from app.models import db, User
+from app.schema import UserSchema
 
 
 @pytest.fixture
 def app():
     app = create_app("testing")
+    yield app
 
-    return app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
 
 
-@pytest.fixture()
+@pytest.fixture
 def database(app):
     with app.app_context():
         db.drop_all()
         db.create_all()
 
     yield db
+
+
+@pytest.fixture
+def new_user_model():
+    return User(
+        first_name="John",
+        last_name="Smith",
+        username="john.smith",
+        email="john.smith@gmail.com",
+        plaintext_password="password",
+    )
+
+
+@pytest.fixture
+def new_user():
+    return {
+        "first_name": "John",
+        "last_name": "Smith",
+        "username": "john.smith",
+        "email": "john.smith@gmail.com",
+        "plaintext_password": "password"
+    }
+
+
+@pytest.fixture
+def user_schema():
+    return UserSchema()
